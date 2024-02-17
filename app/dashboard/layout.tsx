18ca1e05c206dfbacf01be4451bee93f1c127b3c
Lucas Fiduniv <61048699+lucasfiduniv@users.noreply.github.com>
2024-02-17 10:42:36 -0300

    fix: criando a listagem dos menus

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -5,11 +5,14 @@ import Sidebar from "../ui/dashboard/sidebar/sidebar";
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
-    <div>
-      <Sidebar />
-      <div>
+    <div className="flex">
+      <div className=" bg-[#182237] p-4 min-h-screen">
+        <Sidebar />
+      </div>
+      <div className="flex-4 p-4">
         <Navbar />
         {children}
+        {/* <Footer /> */}
       </div>
     </div>
   );
--- app/ui/dashboard/sidebar/sidebar.tsx
+++ app/ui/dashboard/sidebar/sidebar.tsx
@@ -1,4 +1,86 @@
+import {
+  MdAnalytics,
+  MdAttachMoney,
+  MdDashboard,
+  MdHelpCenter,
+  MdOutlineSettings,
+  MdPeople,
+  MdShoppingBag,
+  MdSupervisedUserCircle,
+  MdWork,
+} from "react-icons/md";
+const menuItems = [
+  {
+    title: "Pages",
+    list: [
+      {
+        title: "Dashboard",
+        path: "/dashboard",
+        icon: <MdDashboard />,
+      },
+      {
+        title: "Usuarios",
+        path: "/dashboard/users",
+        icon: <MdSupervisedUserCircle />,
+      },
+      {
+        title: "Produtos",
+        path: "/dashboard/products",
+        icon: <MdShoppingBag />,
+      },
+      {
+        title: "Transacoes",
+        path: "/dashboard/transactions",
+        icon: <MdAttachMoney />,
+      },
+    ],
+  },
+  {
+    title: "Analíticos",
+    list: [
+      {
+        title: "Receita",
+        path: "/dashboard/revenue",
+        icon: <MdWork />,
+      },
+      {
+        title: "Relatórios",
+        path: "/dashboard/reports",
+        icon: <MdAnalytics />,
+      },
+      {
+        title: "Times",
+        path: "/dashboard/teams",
+        icon: <MdPeople />,
+      },
+    ],
+  },
+  {
+    title: "Usuário",
+    list: [
+      {
+        title: "Configuraçoes",
+        path: "/dashboard/settings",
+        icon: <MdOutlineSettings />,
+      },
+      {
+        title: "Ajuda",
+        path: "/dashboard/help",
+        icon: <MdHelpCenter />,
+      },
+    ],
+  },
+];
 const Sidebar = () => {
-  return <div className="font-xl">Sidebar</div>;
+  return (
+    <div className="container sticky top-40">
+      <ul>
+        {menuItems.map((cat) => (
+          <li key={cat.title}>{cat.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
 };
+
 export default Sidebar;
