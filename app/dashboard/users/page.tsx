3516cca7655a82e7c86e1f030f3ed207266e13dd
Lucas Fiduniv <61048699+lucasfiduniv@users.noreply.github.com>
2024-02-19 15:32:58 -0300

    fix: deixando a tela de usuarios mais responsiva e adicionando um shadow

--- app/dashboard/users/page.tsx
+++ app/dashboard/users/page.tsx
@@ -44,81 +44,73 @@ const UsersPage = async ({ searchParams }: UsersPageProps) => {
 
   return (
     <div className="bg-[#182237] p-6 rounded-lg mt-6">
-      <div className="flex items-center justify-between mb-4">
+      <div className="mb-4">
         <Search placeholder="Buscar por um usuário..." />
-        <Link href="/dashboard/users/add">
-          <button className=" px-4 py-2 bg-blue-600 hover:bg-blue-800 text-white rounded-md">
-            Adicionar Novo
-          </button>
-        </Link>
       </div>
-      <table className=" w-full">
-        <thead>
-          <tr>
-            <td>Nome</td>
-            <td>Email</td>
-            <td>Criado Em</td>
-            <td>Função</td>
-            <td>Status</td>
-            <td>Ação</td>
-          </tr>
-        </thead>
-        <tbody>
-          {users.map((user) => (
-            <tr key={user.id}>
-              <td className="p-4">
-                <div className="user flex items-center gap-3">
-                  {user.img ? (
-                    <Image
-                      src={user.img}
-                      alt=""
-                      width={40}
-                      height={40}
-                      className=" rounded-full"
-                    />
-                  ) : (
-                    <Image
-                      src="/avatar.svg"
-                      alt=""
-                      width={40}
-                      height={40}
-                      className=" rounded-full"
-                    />
-                  )}
-                  {user.username}
-                </div>
-              </td>
-              <td>{user.email}</td>
-              <td>{user.createdAt?.toString().slice(4, 16)}</td>
-              <td>{user.isAdmin ? "Administrador" : "Cliente"}</td>
-              <td>
-                <div
-                  className={`${
-                    user.isActive ? "bg-green-500" : "bg-red-500"
-                  } text-white p-2 rounded-md mr-12`}
-                >
-                  {user.isActive ? "ativo" : "inativo"}
-                </div>
-              </td>
-              <td>
-                <div className="buttons flex gap-4">
-                  <Link href={`/dashboard/users/${user.id}`}>
-                    <button className="button view px-4 py-2 bg-teal-500 hover:bg-teal-700 text-white rounded-md">
-                      Ver
-                    </button>
-                  </Link>
-                  {/* <form action={deleteUser}>
-                    <input type="hidden" name="id" value={user.id} />
-                    <button className="button delete px-4 py-2 bg-crimson text-white rounded-md">
-                      Deletar
-                    </button>
-                  </form> */}
-                </div>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      {users.map((user) => (
+        <div
+          key={user.id}
+          className="mb-6 bg-[#182237] p-4 rounded-md hover:shadow-[3px_3px_0px_0px_rgba(0,59,255)] shadow shadow-blue-800"
+        >
+          <div className="flex flex-col md:flex-row md:items-center md:justify-between ">
+            <div className="mb-2 md:mb-0 flex items-center gap-4">
+              {user.img ? (
+                <Image
+                  src={user.img}
+                  alt=""
+                  width={40}
+                  height={40}
+                  className="rounded-full"
+                />
+              ) : (
+                <Image
+                  src="/avatar.svg"
+                  alt=""
+                  width={40}
+                  height={40}
+                  className="rounded-full"
+                />
+              )}
+              <span>{user.username}</span>
+            </div>
+            <div className="flex flex-col md:flex-row md:items-center md:gap-4">
+              <div className="md:mr-4">
+                <span className="text-sm text-gray-400">Email:</span>{" "}
+                <span>{user.email}</span>
+              </div>
+              <div>
+                <span className="text-sm text-gray-400">Criado Em:</span>{" "}
+                <span>{user.createdAt?.toString().slice(4, 16)}</span>
+              </div>
+            </div>
+          </div>
+          <div className="mt-2">
+            <span className="text-sm text-gray-400">Função:</span>{" "}
+            <span>{user.isAdmin ? "Administrador" : "Cliente"}</span>
+          </div>
+          <div className="mt-2">
+            <span className="text-sm text-gray-400">Status:</span>{" "}
+            <span
+              className={`${user.isActive ? "text-green-500" : "text-red-500"}`}
+            >
+              {user.isActive ? "ativo" : "inativo"}
+            </span>
+          </div>
+          <div className="mt-4 flex gap-4">
+            <Link href={`/dashboard/users/${user.id}`}>
+              <button className="px-4 py-2 bg-teal-500 hover:bg-teal-700 text-white rounded-md">
+                Ver
+              </button>
+            </Link>
+            {/* <form action={deleteUser}>
+              <input type="hidden" name="id" value={user.id} />
+              <button className="px-4 py-2 bg-crimson text-white rounded-md">
+                Deletar
+              </button>
+            </form> */}
+          </div>
+        </div>
+      ))}
       <Pagination count={1} />
     </div>
   );
