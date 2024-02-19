24b0514a161b8b28395803b0cc434fa39fcb8c45
Lucas Fiduniv <61048699+lucasfiduniv@users.noreply.github.com>
2024-02-19 13:37:10 -0300

    fix: refatorando a logica da pagina de layout para renderizar a sidebar e os botoes de abrir e fechar

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,13 +1,34 @@
 "use client";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { LayoutProps } from "../interfaces/layoutIntefaces";
 import Navbar from "../ui/dashboard/navbar/navbar";
 import Sidebar from "../ui/dashboard/sidebar/sidebar";
 import Footer from "../ui/dashboard/footer/footer";
 import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
+import { useMediaQuery } from "react-responsive";
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
   const [sidebarOpen, setSidebarOpen] = useState(true);
+  const [isDesktop, setIsDesktop] = useState(true);
+
+  useEffect(() => {
+    const checkScreenWidth = () => {
+      setIsDesktop(window.innerWidth >= 769);
+    };
+
+    checkScreenWidth();
+    window.addEventListener("resize", checkScreenWidth);
+
+    return () => {
+      window.removeEventListener("resize", checkScreenWidth);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (!isDesktop) {
+      setSidebarOpen(false);
+    }
+  }, [isDesktop]);
 
   const toggleSidebar = () => {
     setSidebarOpen(!sidebarOpen);
@@ -24,11 +45,13 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
       </div>
       <div className="flex-1 p-4">
         <Navbar />
-        <button className="md:hidden" onClick={toggleSidebar}>
-          <div className="bg-white rounded-full text-[#182237] p-2 absolute flex">
-            {sidebarOpen ? <FaArrowLeft /> : <FaArrowRight />}
-          </div>
-        </button>
+        {!isDesktop && (
+          <button className="md:hidden" onClick={toggleSidebar}>
+            <div className="bg-white rounded-full text-[#182237] p-2 absolute flex">
+              {sidebarOpen ? <FaArrowLeft /> : <FaArrowRight />}
+            </div>
+          </button>
+        )}
         {children}
         <Footer />
       </div>
