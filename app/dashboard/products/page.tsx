f6d44699cec93625dafdb2ad6344597cfb9f91d2
Lucas Fiduniv <61048699+lucasfiduniv@users.noreply.github.com>
2024-02-24 14:04:35 -0300

    att

--- app/dashboard/products/page.tsx
+++ app/dashboard/products/page.tsx
@@ -1,192 +1,126 @@
-import React from "react";
-import {
-  View,
-  Text,
-  FlatList,
-  StyleSheet,
-  TouchableOpacity,
-} from "react-native";
-import { BarChart, XAxis } from "react-native-svg-charts";
+import Image from "next/image";
+import Link from "next/link";
+import Search from "@/app/ui/dashboard/search/search";
+import Pagination from "@/app/ui/dashboard/pagination/pagination";
+import noproduct from "@/public/noproduct.png";
 
-interface Produto {
-  nome: string;
-  quantidade: number;
+interface SearchParams {
+  q?: string;
+  page?: number;
 }
 
-interface Props {
-  totalVendas: number;
-  valorTotal: number;
-  produtos: Produto[];
-}
+const productsData = [
+  {
+    id: 1,
+    title: "Produto",
+    desc: "descricao",
+    price: 100,
+    createdAt: new Date(),
+    stock: 10,
+    img: "",
+  },
+  {
+    id: 2,
+    title: "Produto 2",
+    desc: "descricao 2",
+    price: 150,
+    createdAt: new Date(),
+    stock: 15,
+    img: "",
+  },
+  {
+    id: 3,
+    title: "Produto 3",
+    desc: "descricao 3",
+    price: 150,
+    createdAt: new Date(),
+    stock: 15,
+    img: "",
+  },
+];
 
-const VendasDoDia = ({ totalVendas, valorTotal }: Props) => {
+const ProductsPage = async ({
+  searchParams,
+}: {
+  searchParams: SearchParams;
+}) => {
+  const q = searchParams?.q || "";
+  const page = searchParams?.page || 1;
+  const count = productsData.length;
+  const ITEMS_PER_PAGE = 1;
+  const startIndex = (page - 1) * ITEMS_PER_PAGE;
+  const endIndex = startIndex + ITEMS_PER_PAGE;
+  const products = productsData.slice(startIndex, endIndex);
+  const totalPages = Math.ceil(count / ITEMS_PER_PAGE);
+  const showPagination = totalPages > 1 && page < totalPages;
+
   return (
-    <View style={styles.card}>
-      <Text style={styles.cardTitle}>Vendas do Dia</Text>
-      <View style={styles.infoContainer}>
-        <Text style={styles.infoText}>Total de Vendas: {totalVendas}</Text>
-        <Text style={styles.infoText}>
-          Valor Total: R$ {valorTotal.toFixed(2)}
-        </Text>
-      </View>
-    </View>
+    <div className="bg-[#182237] p-6 rounded-lg mt-6">
+      <div className="mb-4 flex justify-between items-center">
+        <Search placeholder="Buscar por um produto..." />
+        <Link href="products/add">
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+            Adicionar Usuário
+          </button>
+        </Link>
+      </div>
+      {products.map((product) => (
+        <div
+          key={product.id}
+          className="mb-6 bg-[#182237] p-4 rounded-md hover:shadow-[3px_3px_0px_0px_rgba(0,59,255)] shadow shadow-blue-800"
+        >
+          <div className="flex flex-col md:flex-row md:items-center md:justify-between ">
+            <div className="mb-2 md:mb-0 flex items-center gap-4">
+              {product.img ? (
+                <Image
+                  src={product.img}
+                  alt=""
+                  width={60}
+                  height={60}
+                  className="rounded-full"
+                />
+              ) : (
+                <Image
+                  src={noproduct}
+                  alt=""
+                  width={60}
+                  height={60}
+                  className="rounded-full"
+                />
+              )}
+              <span>{product.title}</span>
+            </div>
+            <div className="flex flex-col md:flex-row md:items-center md:gap-4">
+              <div className="md:mr-4">
+                <span className="text-sm text-gray-400">Descrição:</span>{" "}
+                <span>{product.desc}</span>
+              </div>
+              <div>
+                <span className="text-sm text-gray-400">Preço:</span>{" "}
+                <span>${product.price}</span>
+              </div>
+            </div>
+          </div>
+          <div className="mt-2">
+            <span className="text-sm text-gray-400">Criado Em:</span>{" "}
+            <span>{product.createdAt?.toString().slice(4, 16)}</span>
+          </div>
+          <div className="mt-2">
+            <span className="text-sm text-gray-400">Quantidade:</span>{" "}
+            <span>{product.stock}</span>
+          </div>
+          <div className="mt-4 flex gap-4">
+            <Link href={`/dashboard/products/${product.id}`}>
+              <button className="px-4 py-2 bg-teal-500 hover:bg-teal-700 text-white rounded-md">
+                Ver
+              </button>
+            </Link>
+          </div>
+        </div>
+      ))}
+      <Pagination count={totalPages} />
+    </div>
   );
 };
 
-const ProdutosMaisVendidos = ({ produtos }: Props) => {
-  return (
-    <View style={styles.card}>
-      <Text style={styles.cardTitle}>Produtos Mais Vendidos</Text>
-      <FlatList
-        data={produtos}
-        renderItem={({ item }) => (
-          <View style={styles.produto}>
-            <Text style={styles.produtoNome}>{item.nome}</Text>
-            <Text style={styles.produtoQuantidade}>
-              Vendidos: {item.quantidade}
-            </Text>
-          </View>
-        )}
-        keyExtractor={(item, index) => index.toString()}
-      />
-    </View>
-  );
-};
-
-const VendasPage = () => {
-  const dadosExemplo: Props = {
-    totalVendas: 10,
-    valorTotal: 1000,
-    produtos: [
-      { nome: "Produto A", quantidade: 5 },
-      { nome: "Produto B", quantidade: 3 },
-      { nome: "Produto C", quantidade: 2 },
-    ],
-  };
-
-  // Dados de exemplo para as vendas por dia
-  const vendasPorDia = [10, 20, 15, 25, 30, 20, 15];
-
-  const navigation = useNavigation();
-  const handlePress = () => {
-    navigation.navigate("CriarVenda");
-  };
-
-  return (
-    <View style={styles.container}>
-      <TouchableOpacity style={styles.button} onPress={handlePress}>
-        <Text style={styles.buttonText}>Adicionar Venda</Text>
-      </TouchableOpacity>
-      <VendasDoDia
-        totalVendas={dadosExemplo.totalVendas}
-        valorTotal={dadosExemplo.valorTotal}
-      />
-      <ProdutosMaisVendidos produtos={dadosExemplo.produtos} />
-      <View style={styles.chartContainer}>
-        <Text style={styles.chartTitle}>Vendas por Dia</Text>
-        <BarChart
-          style={{ height: 200 }}
-          data={vendasPorDia}
-          svg={{ fill: "rgb(134, 65, 244)" }}
-          contentInset={{ top: 30, bottom: 30 }}
-        />
-        <XAxis
-          style={{ marginHorizontal: -10, marginTop: 10 }}
-          data={["Seg", "Ter", "Qua", "Qui", "Sex", "Sáb", "Dom"]}
-          formatLabel={(value, index) =>
-            ["Seg", "Ter", "Qua", "Qui", "Sex", "Sáb", "Dom"][index]
-          }
-          contentInset={{ left: 10, right: 10 }}
-          svg={{ fontSize: 10, fill: "black" }}
-        />
-      </View>
-    </View>
-  );
-};
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 20,
-    backgroundColor: "#f0f0f0",
-  },
-  card: {
-    backgroundColor: "#fff",
-    borderRadius: 10,
-    padding: 20,
-    marginBottom: 20,
-    shadowColor: "#000",
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-    shadowOpacity: 0.25,
-    shadowRadius: 3.84,
-    elevation: 5,
-  },
-  cardTitle: {
-    fontSize: 24,
-    fontWeight: "bold",
-    marginBottom: 15,
-  },
-  infoContainer: {
-    backgroundColor: "#f0f0f0",
-    padding: 15,
-    borderRadius: 10,
-  },
-  infoText: {
-    fontSize: 16,
-    marginBottom: 8,
-  },
-  produto: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
-    paddingVertical: 10,
-    borderBottomWidth: 1,
-    borderBottomColor: "#ccc",
-  },
-  produtoNome: {
-    fontSize: 16,
-    fontWeight: "bold",
-  },
-  produtoQuantidade: {
-    fontSize: 16,
-    color: "#666",
-  },
-  button: {
-    backgroundColor: "#00FF00",
-    paddingVertical: 15,
-    paddingHorizontal: 30,
-    borderRadius: 10,
-    alignItems: "center",
-    marginBottom: 30,
-  },
-  buttonText: {
-    color: "#fff",
-    fontSize: 20,
-    fontWeight: "bold",
-  },
-  chartContainer: {
-    backgroundColor: "#fff",
-    borderRadius: 10,
-    padding: 20,
-    marginBottom: 20,
-    shadowColor: "#000",
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-    shadowOpacity: 0.25,
-    shadowRadius: 3.84,
-    elevation: 5,
-  },
-  chartTitle: {
-    fontSize: 24,
-    fontWeight: "bold",
-    marginBottom: 10,
-    textAlign: "center",
-  },
-});
-
-export default VendasPage;
+export default ProductsPage;
--- app/ui/dashboard/rightbar/rightbar.tsx
+++ app/ui/dashboard/rightbar/rightbar.tsx
@@ -17,50 +17,53 @@ const Rightbar = () => {
 
   return (
     <div className={`${isLoading ? "animate-pulse" : ""}`}>
-      <div className="bg-gradient-to-t from-gray-900 to-[#182237] p-6 rounded-lg mb-6 relative">
-        <div className="absolute right-0 bottom-0 w-1/2 h-1/2">
+      <div className="bg-gradient-to-t from-gray-900 to-[#182237] p-6 rounded-lg mb-6 relative flex flex-col-reverse md:flex-row items-center">
+        <div className="flex-grow md:w-1/2">
+          <div className="flex flex-col gap-6">
+            <span className="font-bold text-white">🔥 Disponível Agora</span>
+            <h3
+              className={`text-white text-lg font-semibold ${
+                isLoading ? "bg-gray-700 h-6 w-80 rounded" : ""
+              }`}
+            >
+              {isLoading
+                ? ""
+                : "Como usar a nova versão do painel administrativo?"}
+            </h3>
+            <span
+              className={`text-gray-300 ${
+                isLoading ? "bg-gray-700 h-4 w-36 rounded" : ""
+              }`}
+            >
+              {isLoading ? "" : "Leva 4 minutos para aprender"}
+            </span>
+            <p
+              className={`text-gray-300 ${
+                isLoading ? "bg-gray-700 h-16 w-96 rounded" : ""
+              }`}
+            >
+              {isLoading
+                ? ""
+                : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit eius libero perspiciatis recusandae possimus."}
+            </p>
+            <button
+              className={`flex items-center gap-2 bg-blue-600 hover:bg-blue-800 text-white px-4 py-2 rounded-md ${
+                isLoading ? "hidden" : ""
+              }`}
+            >
+              <MdPlayCircleFilled />
+              Assistir
+            </button>
+          </div>
+        </div>
+        <div className="absolute right-0 bottom-0 md:w-1/2">
           <Image
             className={`object-contain ${isLoading ? "opacity-20" : ""}`}
             src="/astronaut.png"
             alt=""
-            fill
+            width={200}
+            height={200}
           />
-        </div>
-        <div className="flex flex-col gap-6">
-          <span className="font-bold text-white">🔥 Disponível Agora</span>
-          <h3
-            className={`text-white text-lg font-semibold ${
-              isLoading ? "bg-gray-700 h-6 w-80 rounded" : ""
-            }`}
-          >
-            {isLoading
-              ? ""
-              : "Como usar a nova versão do painel administrativo?"}
-          </h3>
-          <span
-            className={`text-gray-300 ${
-              isLoading ? "bg-gray-700 h-4 w-36 rounded" : ""
-            }`}
-          >
-            {isLoading ? "" : "Leva 4 minutos para aprender"}
-          </span>
-          <p
-            className={`text-gray-300 ${
-              isLoading ? "bg-gray-700 h-16 w-96 rounded" : ""
-            }`}
-          >
-            {isLoading
-              ? ""
-              : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit eius libero perspiciatis recusandae possimus."}
-          </p>
-          <button
-            className={`flex items-center gap-2 bg-blue-600 hover:bg-blue-800 text-white px-4 py-2 rounded-md ${
-              isLoading ? "hidden" : ""
-            }`}
-          >
-            <MdPlayCircleFilled />
-            Assistir
-          </button>
         </div>
       </div>
       <div className="bg-gradient-to-t from-gray-900 to-[#182237] p-6 rounded-lg mb-6">
